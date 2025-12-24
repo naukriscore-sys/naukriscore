e2eac3bd033b0e0d04e38d8a4d03360175d3130e
naukriscore-sys <naukriscore-sys>
2025-12-24 17:46:48 +0530

    some changes 6

--- apps/frontend/src/lib/constant.ts
+++ apps/frontend/src/lib/constant.ts
@@ -1,3 +1,7 @@
 // export const HTTP_URL = "https://http-backend.naukriscore.com/api/v2/employee";
 
-export const HTTP_URL = "http://localhost:5001/api/v2/employee";  
+// for dev
+// export const HTTP_URL = "http://localhost:5001/api/v2/employee";  
+
+// for prod
+export const HTTP_URL = "https://naukriscore-h9z7.onrender.com/api/v2/employee";  
